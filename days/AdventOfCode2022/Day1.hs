b024987d221c8a36c081059faf1e870af438a10a
Milan Suk <Milansuk@email.cz>
2022-12-01 06:56:27 +0100

    feat: day1

--- days/AdventOfCode2022/Day1.hs
+++ days/AdventOfCode2022/Day1.hs
@@ -1,4 +1,13 @@
 module AdventOfCode2022.Day1 where
 
-solve :: String -> Int
-solve = length
+import Data.List.Split
+import Data.List
+
+solve1 :: String -> Int
+solve1 xs = maximum $ map sum $ parseLines $ lines xs
+
+solve2 :: String -> Int
+solve2 xs = sum $ take 3 $ reverse $ sort $ map sum $ parseLines $ lines xs
+
+parseLines :: [String] -> [[Int]]
+parseLines xs = map (map read) $ splitOn [""] xs
